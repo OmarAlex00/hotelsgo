9a34e34e5fee8353dcf614919a4d2afe172acaf6
Omar <omarashraffci@gmail.com>
2023-11-12 09:23:44 +0200

    code refactoring to be clean code

--- lib/features/main_feature/first_screen.dart
+++ lib/features/main_feature/first_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hotelsgo/Widgets/first_screen_content.dart';
+import 'package:hotelsgo/features/main_feature/Widgets/first_screen_content.dart';
 
 class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
